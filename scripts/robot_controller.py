692e4290677f8db2074e939a89eac31280ee9b9f
pancakefries <jagarza723@gmail.com>
2022-05-26 01:41:41 -0500

    added arm control and movement to goal

--- scripts/robot_controller.py
+++ scripts/robot_controller.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+from sklearn.preprocessing import PolynomialFeatures
 import rospy 
 import numpy as np
 from robotics_final_project.msg import NodeRow, NodeMatrix
 from pathfinding import Node
 import math
-from geometry_msgs.msg import Vector3, Twist
+from geometry_msgs.msg import Vector3, Twist, Point
+import cv2, cv_bridge
+import moveit_commander
+from sensor_msgs.msg import Image,LaserScan
 
+def convert_hsv (h, s, v):
+    new_h = h/2 - 1
+    new_s = s * 256 - 1
+    new_v = v * 256 - 1
+    return np.array([new_h, new_s, new_v])
+
+color_index = {"green" : 0, "blue": 1, "pink": 2}
+lower_colors = [convert_hsv(75, 0.40, 0.40), convert_hsv(180, 0.40, 0.40), convert_hsv(300, 0.40, 0.40)] 
+upper_colors = [convert_hsv(80, 1, 1), convert_hsv(200, 1, 1), convert_hsv(330, 1, 1)]
 
 class RobotController(object):
     def __init__(self) -> None:
@@ -24,14 +37,202 @@ class RobotController(object):
         self.angspeed_scale = 3
 
         self.received_response = 1
+        self.arrived_at_goal = 0
+        self.goal_type = "flag" # options are flag, player, and goal
+        self.player_returning = 0
+
+        self.current_target = None
+        self.target_in_view = False
+        self.horizontal_error = 0
+        self.distance_error = 0
+        self.arrived_at_target_counter = 0
+
+        self.holding_item = False
+
+        lin = Vector3()
+        ang = Vector3()
+        self.twist = Twist(linear=lin,angular=ang)
 
         rospy.init_node("target_nodes")
         rospy.Subscriber("target_path", NodeMatrix, self.update_goal)
+        rospy.Subscriber("returned", Point, self.player_returned)
+        self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
+        rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+
         # TODO: implement subscriber for node that uses PFL replacement library
         self.node_pub = rospy.Publisher("target_nodes", NodeMatrix, queue_size=10)
         self.vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        self.return_pub = rospy.Publisher("player_return", Point, queue_size=10)
+
+        self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
+        self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
+
+        self.bridge = cv_bridge.CvBridge()
+
         print("RC: initialized")
-    
+
+    def update_movement_flag(self):
+        #print("Horizontal Error: ", self.horizontal_error)
+        #print("Distance Error: ", self.distance_error)
+        # If can see target (cam), turn toward it
+        if (self.target_in_view):
+            turn_speed = (- self.horizontal_error) * 0.4
+            self.twist.angular.z = turn_speed
+            self.twist.linear.x = 0
+        else:
+            self.twist.angular.z = 0.4
+            self.twist.linear.x = 0
+
+        # If target in center, approach
+        if (self.target_in_view and self.horizontal_error < 0.1):
+            forward_speed = (self.distance_error + 0.021) * 0.5
+            self.twist.linear.x = forward_speed
+
+        # If centered and close, increment distance
+        if (self.horizontal_error < 0.1 and self.distance_error == 0):
+            self.arrived_at_target_counter += 1
+        else:
+            self.arrived_at_target_counter = max(0, self.arrived_at_target_counter - 1)
+        
+        print("Counter: ",self.arrived_at_target_counter)
+
+        if (self.arrived_at_target_counter > 100 and self.holding_item == False and self.goal_type == 'flag'):    
+            self.holding_item = True
+            #if self.current_target_type == 'baton':
+            self.pick_up_baton()
+        elif (self.arrived_at_target_counter > 100 and self.holding_item == True and self.goal_type == 'goal'):
+            self.holding_item = False
+            #elif self.current_target_type == 'artag':
+            self.place_baton()
+
+    def image_callback(self, msg):
+        #print("See image")
+        # converts the incoming ROS message to OpenCV format and HSV (hue, saturation, value)
+        if self.goal_type == "flag" and self.arrived_at_goal:
+            image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+            h, w, d = image.shape
+            #cv2.imshow("window", image)
+                    
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            
+            i = color_index[self.current_target]
+
+            # Generate mask
+            mask = cv2.inRange(hsv, lower_colors[i], upper_colors[i])
+
+            M = cv2.moments(mask)
+
+            # If any green pixels are found
+            if M['m00'] > 0:
+                # Center of the green pixels in the image
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+
+                # Visualize red circle in center
+                cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+                self.target_in_view = True
+
+                self.horizontal_error = (cx - w/2) / (w /2)
+
+            else:
+                self.target_in_view = False
+                self.horizontal_error = 10
+            
+            self.update_movement_flag()
+
+        else:
+            self.horizontal_error = 0
+
+    def pick_up_baton(self):
+        print("Picking up baton")
+        # rospy.sleep(3)
+        # TODO: Implement pickup
+        # Move arm back
+        arm_goal = [0.0, np.radians(-93), np.radians(62), np.radians(33)]
+        self.move_group_arm.go(arm_goal, wait=True)
+        self.move_group_arm.stop()
+
+        # Open gripper
+        gripper_goal = [0.019, -0.019]
+        self.move_group_gripper.go(gripper_goal, wait=True)
+        self.move_group_gripper.stop()
+
+        # Stretch arm out to grab 
+        arm_goal = [0.0, np.radians(38), np.radians(-3), np.radians(-29)]
+        self.move_group_arm.go(arm_goal, wait=True)
+        self.move_group_arm.stop()
+        # Move forward to avoid bumping dumbbell in gazebo
+        rospy.sleep(1)
+
+        # Grab dumbbell
+        gripper_goal = [-0.008, 0.008]
+        self.move_group_gripper.go(gripper_goal, wait=True)
+        self.move_group_gripper.stop()
+
+        # Lift up
+        arm_goal = [0.0, np.radians(-23), np.radians(-33), np.radians(33)]
+        self.move_group_arm.go(arm_goal, wait=True)
+        self.move_group_arm.stop()
+
+        self.goal_type = "goal"
+        # self.current_target = self.current_action.tag_id
+        self.target_in_view = False
+        self.arrived_at_target_counter = 0
+        self.arrived_at_goal = False
+        # print("Current target updated to tag: ", self.current_target)
+        return
+
+    def place_baton(self):
+        print("Placing Baton")
+        rospy.sleep(3)
+        # TODO : Implement placement
+        # Extend to place
+        arm_goal = [0.0, np.radians(38), np.radians(-3), np.radians(-29)]
+        self.move_group_arm.go(arm_goal, wait=True)
+        self.move_group_arm.stop()
+        
+        # Open gripper
+        gripper_goal = [0.019, -0.019]
+        self.move_group_gripper.go(gripper_goal, wait=True)
+        self.move_group_gripper.stop()
+
+        # Move arm back to neutral
+        arm_goal = [0.0, 0.0, 0.0, 0.0]
+        self.move_group_arm.go(arm_goal, wait=True)
+        self.move_group_arm.stop()
+
+        gripper_goal = [0, 0]
+        self.move_group_gripper.go(gripper_goal, wait=True)
+        self.move_group_gripper.stop()
+
+        self.holding_item = False
+        self.arrived_at_target_counter = 0
+        self.target_in_view = False
+        self.current_target = None
+        self.goal_type = "player"
+        self.arrived_at_goal = False
+        return
+
+    def scan_callback(self, data):
+        if self.goal_type == "flag" and self.arrived_at_goal:
+            center_average = 0
+            angles = [359, 0, 1]
+            for i in angles:
+                #print(i, ": ", data.ranges[i])
+                if data.ranges[i] == 0.0:
+                    center_average += 0.01
+                if data.ranges[i] > 4:
+                    center_average += 0.01
+                else:
+                    center_average += data.ranges[i]
+            center_average = center_average / len(angles)
+            if center_average != 0.0 and center_average > 0.4:
+                self.distance_error = center_average / 3.5
+            else:
+                self.distance_error = 0
+            self.update_movement()
+            return
+
     def update_goal(self, data):
         self.received_response = 1
         self.goal_x = data.matrix[-1].row[0]
@@ -77,9 +278,20 @@ class RobotController(object):
         # Use a threshold to stop so that the bot doesn't overshoot
         if dist >= 0 and dist <= 0.2:
             self.linspeed = 0
+            self.arrived_at_goal = 1
         else:
             self.linspeed = self.linspeed_max
+            self.arrived_at_goal = 0
         # print("updated movement")
+
+    def grab_flag(self):
+        return
+
+    def drop_flag(self):
+        return
+
+    def player_returned(self, data):
+        self.player_returning = 0
 
     def run(self):
         l = Vector3(x = self.linspeed, y = 0, z = 0)
@@ -88,6 +300,16 @@ class RobotController(object):
             self.decide_current_target()
             self.update_current_pos()
             self.update_movement()
+            if self.arrived_at_goal:
+                if self.goal_type == "player":
+                    self.return_pub.publish(Point(1, 1))
+                    self.player_returning = 1
+                    while self.player_returning:
+                        rospy.sleep(0.2)
+                else:
+                    while self.arrived_at_goal:
+                        rospy.sleep(0.5)
+                self.arrived_at_goal = 0
             l.x = self.linspeed
             a.z = self.angspeed
             self.vel_pub.publish(Twist(linear = l, angular = a))
